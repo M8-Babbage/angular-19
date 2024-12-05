bb1e50e9f1e2ed04dbb928284ab11654c187bf23
Edwin J. Páez <edwinjpr11@gmail.com>
2024-12-04 23:36:45 -0500

    feat: creación de funcionalidad de keyup y click

--- src/app/calculator/components/button/button.component.ts
+++ src/app/calculator/components/button/button.component.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectionStrategy, Component, HostBinding, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, input, output, signal, viewChild } from '@angular/core';
+
+/**
+ * * Para más información sobre los host behaviours leer aquí
+ * ? https://angular.dev/guide/components/host-elements
+ */
 
 @Component({
   selector: 'button-calculator',
@@ -7,16 +12,42 @@ import { ChangeDetectionStrategy, Component, HostBinding, input } from '@angular
   styleUrl: './button.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
-    class: 'w-1/4 border-r border-b border-indigo-400',
+    class: 'border-r border-b border-indigo-400',
+    '[class.w-2/4]': 'isDoubleSize()',
+    '[class.w-1/4]': '!isDoubleSize()',
     // attribute: 'attribute',
     // 'data-size': 'XL',
   },
 })
 export class ButtonComponent {
+  public contentValue = viewChild<ElementRef<HTMLButtonElement>>('button');
+  public onClick = output<string>();
   public isCommand = input<boolean>(false);
   public isDoubleSize = input<boolean>(false);
+  public isPressed = signal<boolean>(false);
 
-  @HostBinding('class.w-2/4') get commandStyle() {
-    return this.isDoubleSize();
+  /**
+   * * Los HostBinding ahora se hacen así '[property]': 'funciones o variables'
+   */
+  // @HostBinding('class.w-2/4') get commandStyle() {
+  //   return this.isDoubleSize();
+  // }
+
+  public handleClick() {
+    const value = this.contentValue()?.nativeElement;
+    if (value) {
+      this.onClick.emit(value.innerText.trim());
+    }
+  }
+
+  public keyboardPressedStyle(key: string) {
+    const value = this.contentValue()?.nativeElement.innerText.trim();
+    if (value === key) {
+      console.log(`La key es: ${key}, el valor del botón es ${value}`);
+      this.isPressed.set(true);
+    }
+    setTimeout(() => {
+      this.isPressed.set(false);
+    }, 150);
   }
 }
--- src/app/calculator/components/calculator/calculator.component.ts
+++ src/app/calculator/components/calculator/calculator.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, viewChildren } from '@angular/core';
 import { ButtonComponent } from '../button/button.component';
 
 @Component({
@@ -6,5 +6,45 @@ import { ButtonComponent } from '../button/button.component';
   imports: [ButtonComponent],
   templateUrl: './calculator.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '(document:keyup)': 'handleKeyboardEvent($event)',
+  },
 })
-export class CalculatorComponent {}
+export class CalculatorComponent {
+  /**
+   * Miembros de clase
+   */
+  public calculatorButtons = viewChildren(ButtonComponent);
+
+  public handleClick(key: string) {
+    console.log('handleClick.key: ', key);
+    this.modifyBackgroundWhenKeyIsLaunched(key);
+  }
+
+  /**
+   * * Antigua forma de usar los HostListener y HostBinding,
+   * * HostBinding: sirve para setear información en el host
+   * * HostListener: permite detectar eventos
+   * ? @HostListener('document:keyup', ['$event'])
+   */
+  public handleKeyboardEvent(event: KeyboardEvent) {
+    const keyEquivalents: Record<string, string> = {
+      Escape: 'C',
+      Clear: 'C',
+      Backspace: 'C',
+      Delete: 'C',
+      Enter: '=',
+      '*': 'x',
+      '/': '÷',
+    };
+    const key = event.key;
+    const keyValueEquivalent = keyEquivalents[key] ?? key;
+    this.handleClick(keyValueEquivalent);
+  }
+
+  public modifyBackgroundWhenKeyIsLaunched(key: string) {
+    this.calculatorButtons().forEach((button) => {
+      button.keyboardPressedStyle(key);
+    });
+  }
+}
